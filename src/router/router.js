207b09c044b805440a0947668891bceadd8abe39
liupan <863420364@qq.com>
2019-04-10 15:42:43 +0800

    home

--- src/router/router.js
+++ src/router/router.js
@@ -2,6 +2,13 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom'
 import App from '../App'
 import Home from '../views/Home/home'
+import Global from '../views/Global/global'
+import Muandbaby from '../views/Muandbaby/muandbaby'
+import Luxury from '../views/Luxury/luxury'
+import Pop from '../views/Pop/pop'
+
+
+
 import Together from '../views/Together/together'
 import Shopcar from '../views/Shopcar/shopcar'
 import My from '../views/My/my'
@@ -11,6 +18,12 @@ export const routes = (
         <App>
             <Switch>
                 <Route path="/home" component={Home}></Route>
+                <Route path="/global" component={Global}></Route>
+                <Route path="/muandbaby" component={Muandbaby}></Route>
+                <Route path="/luxury" component={Luxury}></Route>
+                <Route path="/pop" component={Pop}></Route>
+
+
                 <Route path="/together" component={Together}></Route>
                 <Route path="/shopcar" component={Shopcar}></Route>
                 <Route path="/my" component={My}></Route>
--- src/views/Global/global.js
+++ src/views/Global/global.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import style from './home.module.scss'
 import Headerbar from '../../components/Headerbar/headerbar';
 import Navbar from '../../components/Navbar/navbar';
-class Home extends React.Component{
+import style from './global.module.scss'
+class Global extends React.Component{
     render(){
         return(
             <div>
@@ -13,4 +13,4 @@ class Home extends React.Component{
         )
     }
 }
-export default Home
+export default Global
