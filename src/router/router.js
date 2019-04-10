0112bb27ca830053f521093acc62ee0fad8d4189
liupan <863420364@qq.com>
2019-04-10 14:15:05 +0800

    配置

--- src/router/router.js
+++ src/router/router.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom'
 import App from '../App'
 import Home from '../views/Home/home'
 import Together from '../views/Together/together'
@@ -9,10 +9,13 @@ import My from '../views/My/my'
 export const routes = (
     <Router>
         <App>
-            <Route path="/home" component={Home}></Route>
-            <Route path="/together" component={Together}></Route>
-            <Route path="/shopcar" component={Shopcar}></Route>
-            <Route path="/my" component={My}></Route>
+            <Switch>
+                <Route path="/home" component={Home}></Route>
+                <Route path="/together" component={Together}></Route>
+                <Route path="/shopcar" component={Shopcar}></Route>
+                <Route path="/my" component={My}></Route>
+                <Redirect from="/" to="/home"></Redirect>
+            </Switch>
         </App>
     </Router>
 )
